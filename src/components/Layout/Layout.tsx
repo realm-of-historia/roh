d3090e52072e0bf670524e7ac141d2544e8a8c5b
vadim <115945880+Vadim-Shipnevsky@users.noreply.github.com>
2023-10-17 01:53:34 +0300

    refactor: adding data to Footer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,12 +5,14 @@ import { useApiFetch } from "@/composable/useApiFetch";
 
 export default async function Layout({children}: {children: any}) {
     const dataHeader = await useApiFetch('api/header?populate=*')
+    const dataDigest = await useApiFetch('api/footer?populate[socialNetwork][populate]=*')
+
     console.log('Header', dataHeader)
     return(
         <div>
             <Header data={dataHeader.data.attributes.link}/>
             {children}
-            <Digest></Digest>
+            <Digest data={dataDigest.data.attributes}></Digest>
         </div>
     )
 }
