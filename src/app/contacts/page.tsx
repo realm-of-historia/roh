68da6cfff600219c5ef16921ed4f705109c478e3
vadim <115945880+Vadim-Shipnevsky@users.noreply.github.com>
2023-10-17 23:13:09 +0300

    displayed data in contacts

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -5,11 +5,11 @@ import Layout from '@/components/Layout/Layout'
 import { useApiFetch } from '@/composable/useApiFetch'
 
 export default async function ContactsPage() {
-    const data = await useApiFetch('api/blog-page?populate[video][populate]=*&populate[cardBundles][populate]=*&populate[collectionOfPictures][populate]=*&populate[ribbon][populate]=*&populate[instagramPostText][populate]=*')
+    const data = await useApiFetch('api/contacts-page?populate=*')
 
     return (
         <div style={{ overflowX: 'hidden' }}>
-            <ContactsView></ContactsView>
+            <ContactsView data={data} />
         </div>
     )
 }
