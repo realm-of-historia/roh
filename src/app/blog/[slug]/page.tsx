f5b2d3741d4f20c06a71a0d9f8a0a2352dc098b4
vadim <115945880+Vadim-Shipnevsky@users.noreply.github.com>
2023-10-21 01:14:21 +0300

    refactor: use data in blog

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -8,21 +8,27 @@ import Comments from '@/views/HomePage/screens/Comments/Comments'
 import Bundles from '@/views/HomePage/screens/Bundles/Bundles'
 import Digest from '@/components/Digest/Digest'
 import { useApiFetch } from '@/composable/useApiFetch'
+import { useSectionData } from '@/composable/useSectionData'
 export interface StandardComponentProps {
     slug?: string,
     params?: any
 }
 export default async function ArticlePage(slug :StandardComponentProps) {
     console.log( slug.params.slug)
-    const data = await useApiFetch('api/blog-article?populate=*')
-    const dataArticle = await useApiFetch(`api/articles?filters[uid][$eq]=${slug.params.slug}`)
-
+    const data = await useApiFetch('api/blog-article?populate[cardBundles][populate]=*&populate[listOfCategories][populate]=*&populate[ribbon][populate]=*')
+    const dataArticleLast = await useApiFetch(`api/articles?pagination[pageSize]=4&fields[0]=title&fields[1]=uid`)
+    const dataArticle = await useApiFetch(`api/articles?filters[uid][$eq]=${slug.params.slug}&populate=*`)
+    const cardBundlesText = useSectionData(data, 'cardBundlesText')
+    const cardBundlesHref = useSectionData(data, 'cardBundlesHref')
+    const cardBundles = useSectionData(data, 'cardBundles')
+    const ribbon = useSectionData(data, 'ribbon')
+    console.log(cardBundles)
     console.log(dataArticle)
     return(
         <div>
-            <GetStarted></GetStarted>
+            <GetStarted data={data} article={dataArticle} dataArticleLast={dataArticleLast}></GetStarted>
             <Comments></Comments>
-            <Bundles></Bundles>
+            <Bundles ribbon={ribbon} data={cardBundles} text={cardBundlesText} href={cardBundlesHref}></Bundles>
         </div>
     )
 }
