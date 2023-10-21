2bb85e2c1f210950a5434be73bffe60a75058356
vadim <115945880+Vadim-Shipnevsky@users.noreply.github.com>
2023-10-21 20:38:26 +0300

    refactor: passed the data to the Artist component

--- src/views/ArtistPage/ArtistPage/ArtistPage.tsx
+++ src/views/ArtistPage/ArtistPage/ArtistPage.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import styles from './ArtistPage.module.scss'
 import Text from "@/components/Text/Text";
 import Creatures from "../Creatures/Creatures";
+import ImageMy from "@/components/Image/ImageMy";
 
 
-export default function Artist({name, spec, text, item}: {name: string | Array<string>, spec: string | Array<string>, text: string | Array<string>, item: string | Array<string>}){
+export default function Artist({name, spec, text, item, avatar}: {name: string | Array<string>, spec: string | Array<string>, text: string | Array<string>, item: string | Array<string>, avatar? : any}){
 
 
 
@@ -12,7 +13,8 @@ export default function Artist({name, spec, text, item}: {name: string | Array<s
         <div className={styles.artist}>
             <div className={styles.left}>
                 <div className={styles.dividerLeft}></div>
-                <img src='userImage.png' width={363} height={363} alt=''/>
+                {/* <img src='userImage.png' width={363} height={363} alt=''/> */}
+                <ImageMy src={avatar} width={363} height={363} alt=''/>
                 <div className={styles.info}>
                     <Text>
                         <p className={styles.name}>
@@ -25,13 +27,18 @@ export default function Artist({name, spec, text, item}: {name: string | Array<s
                         </p>
                     </Text>
                     <div className={styles.container}>
-                        {Array.isArray(text) ? text.map((element: any, index: number) => (
+                        {/* {Array.isArray(text) ? text.map((element: any, index: number) => (
                             <Text key={index}>
                                 <p className={styles.text}>
                                     {element}
                                 </p>
                             </Text>
-                        )) : <Text><p className={styles.text}>{text}</p></Text>}
+                        )) : <Text><p className={styles.text}>{text}</p></Text>} */}
+                        <Text >
+                                <p className={styles.text}>
+                                    {text}
+                                </p>
+                        </Text>
                     </div>
                 </div>
             </div>
