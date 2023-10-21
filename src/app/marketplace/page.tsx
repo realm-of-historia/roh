53c06fd088ccc93a941fdff08c27c35b17c554fb
vadim <115945880+Vadim-Shipnevsky@users.noreply.github.com>
2023-10-21 20:44:27 +0300

    refactor: passed the data to the MarketplaceTitle component

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -1,11 +1,12 @@
+import { useApiFetch } from '@/composable/useApiFetch'
 import MarketplaceView from '@/views/MarketplaceView/MarketplaceView'
 import React from 'react'
 
 
 
-export default function MarketplacePage() {
-    
+export default async function MarketplacePage() {
+    const data = await useApiFetch('api/marketplace?populate=*')
     return(
-        <MarketplaceView></MarketplaceView>
+        <MarketplaceView data={data} />
     )
 }
