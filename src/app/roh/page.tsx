0741f9c203838bb8194a0ddd2185f2d967d7c390
vadim <115945880+Vadim-Shipnevsky@users.noreply.github.com>
2023-11-09 22:52:31 +0300

    feat: add new Problem component

--- src/app/roh/page.tsx
+++ src/app/roh/page.tsx
@@ -1,5 +1,6 @@
 import WrapperTexture from "@/components/WrapperTexture/WrapperTexture"
 import HistoriaMission from "@/components/rohComponents/HistoriaMission/HistoriaMission"
+import Problem from "@/components/rohComponents/Problem/Problem"
 import RealmHistory from "@/components/rohComponents/RealmHistory/RealmHistory"
 import { useApiFetch } from "@/composable/useApiFetch"
 import { useSectionData } from "@/composable/useSectionData"
@@ -9,6 +10,8 @@ import AboutTheProjectWrapper from "@/views/HomePage/screens/AboutTheProject/Abo
 export default async function Roh() {
     const data = await useApiFetch('api/kingdom-of-history?populate[missionTable][populate]=*&populate[tableProblem][populate]=*&populate[tableSolution][populate]=*&populate[ribbon][populate]=*&populate[scheme][populate]=*&populate[mobileDiagram][populate]=*&populate[journeyImg][populate]=*&populate[DescriptionTravelPicture][populate]=*&populate[TheAtriumTable][populate]=*&populate[slogan][populate]=*')
     const missionTable = useSectionData(data, 'missionTable')
+    const tableProblem = useSectionData(data, 'tableProblem')
+    const headerProblem = useSectionData(data, 'headerProblem')
 
     return (
         <>
@@ -16,6 +19,7 @@ export default async function Roh() {
                 <RealmHistory header={data?.data.attributes.header} welcome={data?.data.attributes.welcome}/>
                 <AboutTheProjectWrapper />
                 <HistoriaMission data={missionTable}/>
+                <Problem data={tableProblem} header={headerProblem}/>
             </WrapperTexture>
         </>
     )
