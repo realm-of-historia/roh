72d5329298e00d492bceb7084942ef70dc83de6d
vadim <115945880+Vadim-Shipnevsky@users.noreply.github.com>
2023-11-11 20:48:40 +0300

    feat: add component HeaderHistory

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import HistoryView from '@/views/UserPage/screens/History/HistoryView'
+import { useApiFetch } from '@/composable/useApiFetch'
+import { useSectionData } from '@/composable/useSectionData'
+import HeaderHistory from '@/components/HistoryPageComponent/HeaderHistory/HeaderHistory'
+import HeritageDefault from '@/views/HomePage/screens/Heritage/HeritageDefault'
+import WrapperTexture from '@/components/WrapperTexture/WrapperTexture'
 
 
-export default function HistoryPage() {
+export default async function HistoryPage() {
 
+    const data = await useApiFetch('api/history?&populate[historyTable][populate]=*&populate[carahunge][populate]=*')
+    const header = useSectionData(data, 'header')
+    const subtitle = useSectionData(data, 'subtitle')
+    const dataHeaderHistory = {
+        header, subtitle
+    }
 
-
-    return(
-        <HistoryView></HistoryView>
+    return (
+        <>
+            <WrapperTexture>
+                <HeaderHistory data={dataHeaderHistory} />
+                
+                <HeritageDefault />
+            </WrapperTexture>
+        </>
+        // <HistoryView></HistoryView> // раньше была страница
     )
 }
