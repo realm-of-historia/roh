def6fd5aeed1f04ef6a3b8bb72c4c07ae4fc5f06
vadim <115945880+Vadim-Shipnevsky@users.noreply.github.com>
2023-11-11 21:40:03 +0300

    feat: add component CarahungeHistory

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -5,6 +5,7 @@ import { useSectionData } from '@/composable/useSectionData'
 import HeaderHistory from '@/components/HistoryPageComponent/HeaderHistory/HeaderHistory'
 import HeritageDefault from '@/views/HomePage/screens/Heritage/HeritageDefault'
 import WrapperTexture from '@/components/WrapperTexture/WrapperTexture'
+import CarahungeHistory from '@/components/HistoryPageComponent/CarahungeHistory/CarahungeHistory'
 
 
 export default async function HistoryPage() {
@@ -12,6 +13,8 @@ export default async function HistoryPage() {
     const data = await useApiFetch('api/history?&populate[historyTable][populate]=*&populate[carahunge][populate]=*')
     const header = useSectionData(data, 'header')
     const subtitle = useSectionData(data, 'subtitle')
+    const historyTable = useSectionData(data, 'historyTable')
+
     const dataHeaderHistory = {
         header, subtitle
     }
@@ -20,7 +23,7 @@ export default async function HistoryPage() {
         <>
             <WrapperTexture>
                 <HeaderHistory data={dataHeaderHistory} />
-                
+                <CarahungeHistory data={historyTable}/>
                 <HeritageDefault />
             </WrapperTexture>
         </>
