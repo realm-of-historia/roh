7cae66afb131719f165f46ce4783af7f24d49115
vadim <115945880+Vadim-Shipnevsky@users.noreply.github.com>
2023-11-12 13:38:26 +0300

    feat: add a mission to the page

--- src/views/HomePage/screens/AboutTheProject/AboutTheProject.tsx
+++ src/views/HomePage/screens/AboutTheProject/AboutTheProject.tsx
@@ -24,6 +24,7 @@ const AboutTheProject = () => {
 
     return (
         <div className={styles.container}>
+            <Divider position={"bottom right"} horizontal={true}/>
             {
              dataTheProject ?
                     <>
