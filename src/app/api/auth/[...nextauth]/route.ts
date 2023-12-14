67ded7c5bb9f79c952773584174b16a3acd5944e
Jelmond <ant.soloviov@gmail.com>
2023-12-14 18:45:09 +0300

    feat: add next-auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,49 @@
+import NextAuth from "next-auth"
+import DiscordProvider from "next-auth/providers/discord"
+import GoogleProvider from 'next-auth/providers/google'
+import { WalletService } from "@/services/WalletService"
+
+
+export const authOptions: any = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_ID ?? "",
+      clientSecret: process.env.DISCORD_SECRET ?? "",
+    }),
+    GoogleProvider({
+        clientId: process.env.GOOGLE_ID ?? "",
+        clientSecret: process.env.GOOGLE_SECRET ?? "",
+    })
+  ],
+  session: {
+    jwt: true,
+  },
+  callbacks: {
+  async signIn({ user, isNewUser }: {user: any, isNewUser: any}) {
+    if (isNewUser) {
+      console.info(`NextAuth: created user ${user.id}`);
+      await WalletService.createWallets(user.id);
+      console.info(`NextAuth: initialized wallet for user ${user.id}`);
+      console.log(user)
+    }
+
+    console.info(`NextAuth: fetching wallets for user ${user.id}`);
+    user.wallets = await WalletService.fetchWallets(user.id);
+    return true;
+  },
+  async jwt({ token, user, account, profile, isNewUser }: { token: any, user: any, account: any, profile: any, isNewUser: any }) {
+    if (user) {
+      token.wallets = user.wallets;
+    }
+    return token;
+  },
+  async session({ session, token }: {session: any, token: any}) {
+    session.wallets = token.wallets;
+    return session;
+  },
+}
+}
+
+export const handler = NextAuth(authOptions)
+
+export {handler as GET, handler as POST};
