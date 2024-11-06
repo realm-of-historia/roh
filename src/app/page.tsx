b764732be1dbfba18262cfb350c3cd0d8641b74e
narekpetrosyan <narek.petrosyan988@gmail.com>
2024-11-06 17:56:12 +0400

    feature(2817): Change menu items, feature(2812): Update homepage to carahunge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,81 @@
+import InitiationX from "@/components/CarahungeXPageComponents/InitiationX/InitiationX";
+import Preserve from "@/components/CarahungeXPageComponents/Preserve/Preserve";
+import RealmOfHistory from "@/components/CarahungeXPageComponents/RealmOfHistory/RealmOfHistory";
+import TableCarahunge from "@/components/CarahungeXPageComponents/TableCarahunge/TableCarahunge";
+import HashAnchor from "@/components/HashAnchor/HashAnchor";
+import Panegliph from "@/components/Panegliph/Panegliph";
+import RunningLine from "@/components/RunningLine/RunningLine";
+import WrapperTexture from "@/components/WrapperTexture/WrapperTexture";
+import { useApiFetch } from "@/composable/useApiFetch";
+import { useSectionData } from "@/composable/useSectionData";
+import CollectionOfWorks from "@/views/HomePage/screens/CollectionOfWorks/CollectionOfWorks";
+import Heritage from "@/views/HomePage/screens/Heritage/Heritage";
+import HeritageDefault from "@/views/HomePage/screens/Heritage/HeritageDefault";
+
 import styles from './page.module.scss'
 import '../assets/index.scss'
 import 'react-toastify/dist/ReactToastify.css';
-import HomeView from '@/views/HomePage/HomeView';
-import {useApiFetch} from '../composable/useApiFetch.js'
-
 
 export default async function Home() {
-  const data = await useApiFetch('api/home-page?&populate[videoArticles][populate]=*&populate[swiperTop][populate]=*&populate[article][populate]=*&populate[ribbon][populate]=*&populate[articles][populate]=*&populate[article_populars][populate]=*&populate[selfSustainableHeritage][populate]=*&populate[map][populate]=*&populate[ribbon2][populate]=*&populate[videoArticlesPoster][populate]=*')
+    const data = await useApiFetch('api/carahunge-x?&populate[table][populate]=*&populate[ribbon][populate]=*&populate[ribbon2][populate]=*&populate[initiationTable][populate]=*&populate[selfSustainableHeritage][populate]=*')
+    const header = useSectionData(data, 'header')
+    const subtitle = useSectionData(data, 'subtitle')
+    const preserveHeader = useSectionData(data, 'preserveHeader')
+    const preserveDescription = useSectionData(data, 'preserveDescription')
+    const table = useSectionData(data, 'table')
+    const ribbon = useSectionData(data, 'ribbon')
+    const ribbon2 = useSectionData(data, 'ribbon2')
+    const initiationTitle = useSectionData(data, 'initiationTitle')
+    const initiationSubtitle = useSectionData(data, 'initiationSubtitle')
+    const initiationTable = useSectionData(data, 'initiationTable')
+    const selfSustainableHeritage = useSectionData(data, 'selfSustainableHeritage')
+
+    let dataPreserve = {
+        preserveHeader, preserveDescription
+    }
+    let dataRealmOfHistory = {
+        header,
+        subtitle,
+    }
+    let dataInitiationX = {
+        initiationTitle, initiationSubtitle, initiationTable
+    }
 
   return (
     <main className={styles.main}>
-      <HomeView data={data}></HomeView>
+      <WrapperTexture>
+        <HashAnchor />
+      </WrapperTexture>
+      <WrapperTexture>
+        <RealmOfHistory data={dataRealmOfHistory} />
+      </WrapperTexture>
+      <WrapperTexture>
+        <Preserve data={dataPreserve} />
+      </WrapperTexture>
+      <WrapperTexture>
+        <Panegliph isFirst={false} />
+      </WrapperTexture>
+      <WrapperTexture>
+        <TableCarahunge data={table} />
+      </WrapperTexture>
+      <WrapperTexture>
+        <RunningLine image={ribbon?.data.attributes.url} />
+      </WrapperTexture>
+      <WrapperTexture>
+        <InitiationX data={dataInitiationX} roadColumn={true}/>
+      </WrapperTexture>
+      <WrapperTexture>
+        <RunningLine image={ribbon2?.data.attributes.url} />
+      </WrapperTexture>
+      <WrapperTexture>
+        <CollectionOfWorks />
+      </WrapperTexture>
+      <WrapperTexture>
+        <Heritage data={selfSustainableHeritage} />
+      </WrapperTexture>
+      <WrapperTexture>
+        <HeritageDefault />
+      </WrapperTexture>
     </main>
   )
 }
